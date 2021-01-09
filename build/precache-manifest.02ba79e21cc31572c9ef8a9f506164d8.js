09d5bb3e2f07407d8a23c4af577b51a98ce07509
Alina Ilina <alinailina.random@gmail.com>
2021-01-09 23:02:08 +0200

    Create new production build & deploy

---
+++ build/precache-manifest.02ba79e21cc31572c9ef8a9f506164d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f093deb0050c04b4f56b695e9eb74f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a571220c653a730dc7a5",
+    "url": "/static/css/main.fe90ca0f.chunk.css"
+  },
+  {
+    "revision": "47079655076aeb1cd684",
+    "url": "/static/js/2.6fc201f7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.6fc201f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a571220c653a730dc7a5",
+    "url": "/static/js/main.36ff4ca1.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
